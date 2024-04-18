9d33e5e5e90876ea08a63d2d63825770dbd4381c
Uppal <26uppalmaxwell@gmail.com>
2024-04-18 14:16:22 -0400

    Added Hello World script from Wiki

--- Sandbox.js
+++ Sandbox.js
@@ -1,10 +1,14 @@
 // Programmer: Max Uppal
-// Team Member: Ethan Vanlandegent
-// Bolt ID: 
+// Team Member: Ethan VanLandegent
+// Bolt ID: SB-EC76
 // Date: 4-16-24
 // Program: Hello World - Sandbox
 // Program URL: https://edu.sphero.com/program/16700420/edit
 
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
