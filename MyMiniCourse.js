393aef0881584ed0c29f6108de1362821accd584
Uppal <26uppalmaxwell@gmail.com>
2024-04-23 13:51:44 -0400

    Got to first black pen checkpoint

--- MyMiniCourse.js
+++ MyMiniCourse.js
@@ -2,9 +2,9 @@
 // Team Member: Ethan VanLandegent
 // Bolt ID: SB-EC76
 // Date: 4-23-24
-// Program: Hello World - My Mini Course
-// Program URL: https://edu.sphero.com/program/16743348/edit
+// Program: Hello World - Sandbox
+// Program URL: https://edu.sphero.com/program/16700420/edit
 
 async function startProgram() {
-	// Write code here
+		await roll((getHeading() + 45), 120, 1);
 }
