63fe1b37c0c4147eae052030eca810e9948810a9
Uppal <26uppalmaxwell@gmail.com>
2024-04-23 13:35:49 -0400

    Added Basic Information to MyMiniCourse

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Max Uppal
+// Team Member: Ethan VanLandegent
+// Bolt ID: SB-EC76
+// Date: 4-23-24
+// Program: Hello World - My Mini Course
+// Program URL: https://edu.sphero.com/program/16743348/edit
+
+async function startProgram() {
+	// Write code here
+}
