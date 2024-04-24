8498309a1a97b256fe34fe6b38d3d9a4acd93e7e
Uppal <26uppalmaxwell@gmail.com>
2024-04-24 13:56:35 -0400

    Reached 1st Checkpoint

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -2,5 +2,10 @@
 // Team Member: Ethan VanLandegent
 // Bolt ID: SB-EC76
 // Date: 4-24-24
-// Program: Hello World - Sandbox
+// Program: Obstacle Course - Sandbox
 // Program URL: https://edu.sphero.com/program/16752514/edit
+
+async function startProgram() {
+	//Maxwell Uppal
+	await roll((getHeading() + 35), 120, 1.57);
+}
