2383237b867a77bf6e9ef53734abb2011c6376b8
Uppal <26uppalmaxwell@gmail.com>
2024-04-30 14:18:41 -0400

    Blue LED Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,17 +6,18 @@
 // Program URL: https://edu.sphero.com/program/16788332/edit
 
 async function startProgram() {
-	// Programmer: Maxwell Uppal - Start Text & Audio Checkpoint Commit - Date:
+	// Programmer: Maxwell Uppal - Start Text & Audio Checkpoint Commit - Date: 4/30/24
 	await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 	await speak("Start", true);
 	
 
-	// Programmer:               - Blue LED Checkpoint Commit - Date: 
-
+	// Programmer: Ethan Vanlandegent - Blue LED Checkpoint Commit - Date: 4/30/24
+	await roll((getHeading() + 0), 50, 3.5);
+	setMainLed({ r: 0, g: 0, b: 255 });
 
 
 	// Programmer:               - Sound One Checkpoint Commit - Date:
-
+	
 
 
 	// Programmer:               - Red LED Checkpoint Commit - Date: 
