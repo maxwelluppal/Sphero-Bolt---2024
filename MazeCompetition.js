3e852ea4074e0ebcc0372845e3a7b0ef87e2c4b6
Uppal <26uppalmaxwell@gmail.com>
2024-04-30 13:21:18 -0400

    Initial Maze Competition Java Script File

---
+++ MazeCompetition.js
@@ -0,0 +1,6 @@
+// Programmer: Ethan VanLandegent
+// Team Member: Max Uppal
+// Bolt ID: SB-EC76
+// Date: 4-30-24
+// Program: Maze Competition 
+// Program URL: https://edu.sphero.com/program/16752539/edit
