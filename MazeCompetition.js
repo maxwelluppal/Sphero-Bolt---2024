615b963143c073899112ccc80d9916e1109671f4
Uppal <26uppalmaxwell@gmail.com>
2024-04-30 14:02:43 -0400

    Start Text & Audio Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,34 @@
 // Program URL: https://edu.sphero.com/program/16788332/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Maxwell Uppal - Start Text & Audio Checkpoint Commit - Date:
+
+
+
+	// Programmer:               - Blue LED Checkpoint Commit - Date: 
+
+
+
+	// Programmer:               - Sound One Checkpoint Commit - Date:
+
+
+
+	// Programmer:               - Red LED Checkpoint Commit - Date: 
+
+
+
+	// Programmer:               - Sound Two Checkpoint Commit - Date: 
+
+
+
+	// Programmer:               - Green LED Checkpoint Commit - Date: 
+
+
+
+	// Programmer:               - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+	// Programmer:               - Finish Text & Audio Commit - Date: 
+
 }
