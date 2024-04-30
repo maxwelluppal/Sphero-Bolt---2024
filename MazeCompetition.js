e443d98101c1fb45f4e3a74864475197e1a15dc2
Uppal <26uppalmaxwell@gmail.com>
2024-04-30 13:28:29 -0400

    Added Beginning Comments & Start Program JS

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -3,4 +3,8 @@
 // Bolt ID: SB-EC76
 // Date: 4-30-24
 // Program: Maze Competition 
-// Program URL: https://edu.sphero.com/program/16752539/edit
+// Program URL: https://edu.sphero.com/program/16788332/edit
+
+async function startProgram() {
+	// Write code here
+}
