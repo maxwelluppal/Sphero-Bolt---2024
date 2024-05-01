db0df36ea8a87792bbc9b4a5101d439efb270ca8
Uppal <26uppalmaxwell@gmail.com>
2024-05-01 14:18:40 -0400

    Green LED Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -12,14 +12,14 @@ async function startProgram() {
 	
 
 	// Programmer: Ethan Vanlandegent - Blue LED Checkpoint Commit - Date: 4/30/24
-	await roll((getHeading() + 0), 50, 3.65);
+	await roll((getHeading() + 0), 50, 3.55);
 	await delay(1)
 	setMainLed({ r: 0, g: 0, b: 255 });
 	await delay(1)
 
 
 	// Programmer: Maxwell Uppal - Sound One Checkpoint Commit - Date:4/30/24
-	await roll((getHeading() + 90), 50, 2.6);
+	await roll((getHeading() + 90), 50, 2.43);
 	await delay(1)
 	await Sound.ScienceFiction.Blaster.play(true);
 	await delay(1)
@@ -27,22 +27,26 @@ async function startProgram() {
 
 
 	// Programmer: Ethan Vanlandegent - Red LED Checkpoint Commit - Date:5/1/24 
-	await roll((getHeading() + 90), 50, 1.8);
+	await roll((getHeading() + 90), 50, 1.65);
 	await delay(1)
-	await roll((getHeading() + 43), 50, 1.3);
+	await roll((getHeading() + 40), 50, 1.65);
 	await delay(1)
+	setMainLed({ r: 255, g: 0, b: 0 });
 
 
 	// Programmer: Maxwell Uppal - Sound Two Checkpoint Commit - Date:5/1/24
-	await roll((getHeading() - 90), 50, .9);
+	await roll((getHeading() - 95), 50, 1);
 	await delay(1)
-	await roll((getHeading() - 90), 50, 1.6);
+	await roll((getHeading() - 90), 50, 1.75);
 	await delay(1)
 	await Sound.ScienceFiction.Blaster.play(true);
 
 
-	// Programmer:               - Green LED Checkpoint Commit - Date: 
-
+	// Programmer:Ethan VanLandegent - Green LED Checkpoint Commit - Date: 5/1/24
+	await roll((getHeading() + 90), 50, 1.4);
+	await delay(1)
+	await roll((getHeading() + 45), 50, .65);
+	setMainLed({ r: 0, g: 255, b: 0 });
 
 
 	// Programmer:               - Purple LED & Sound Three Checkpoint Commit - Date:
@@ -50,5 +54,4 @@ async function startProgram() {
 
 
 	// Programmer:               - Finish Text & Audio Commit - Date: 
-
 }
