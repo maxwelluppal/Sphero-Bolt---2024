02a18d9f3b77d8e126fd8dbb9a0b736b49cf6d02
Uppal <26uppalmaxwell@gmail.com>
2024-05-02 13:35:10 -0400

    Purple LED & Sound 3 Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -12,46 +12,52 @@ async function startProgram() {
 	
 
 	// Programmer: Ethan Vanlandegent - Blue LED Checkpoint Commit - Date: 4/30/24
-	await roll((getHeading() + 0), 50, 3.55);
+	await roll((getHeading() + 0), 50, 3.4);
 	await delay(1)
 	setMainLed({ r: 0, g: 0, b: 255 });
 	await delay(1)
 
 
 	// Programmer: Maxwell Uppal - Sound One Checkpoint Commit - Date:4/30/24
-	await roll((getHeading() + 90), 50, 2.43);
+	await roll((getHeading() + 90), 50, 2.45);
 	await delay(1)
-	await Sound.ScienceFiction.Blaster.play(true);
+	await Sound.ScienceFiction.Blaster.play(true); 
 	await delay(1)
 	
 
 
 	// Programmer: Ethan Vanlandegent - Red LED Checkpoint Commit - Date:5/1/24 
-	await roll((getHeading() + 90), 50, 1.65);
+	await roll((getHeading() + 90), 50, 1.6);
 	await delay(1)
-	await roll((getHeading() + 40), 50, 1.65);
+	await roll((getHeading() + 38), 50, 1.45);
 	await delay(1)
-	setMainLed({ r: 255, g: 0, b: 0 });
 
 
 	// Programmer: Maxwell Uppal - Sound Two Checkpoint Commit - Date:5/1/24
-	await roll((getHeading() - 95), 50, 1);
+	await roll((getHeading() - 90), 50, .9);
 	await delay(1)
-	await roll((getHeading() - 90), 50, 1.75);
+	await roll((getHeading() - 90), 50, 1.4);
 	await delay(1)
 	await Sound.ScienceFiction.Blaster.play(true);
 
 
 	// Programmer:Ethan VanLandegent - Green LED Checkpoint Commit - Date: 5/1/24
-	await roll((getHeading() + 90), 50, 1.4);
+	await roll((getHeading() + 90), 50, 1.2);
 	await delay(1)
-	await roll((getHeading() + 45), 50, .65);
+	await roll((getHeading() + 45), 50, .75);
+	await delay(1)
 	setMainLed({ r: 0, g: 255, b: 0 });
-
-
-	// Programmer:               - Purple LED & Sound Three Checkpoint Commit - Date:
-
-
-
+	// Programmer: Maxwell Uppal - Purple LED & Sound Three Checkpoint Commit - Date:5/2/24
+	await roll((getHeading() - 90), 50, 0,9);
+	await delay(1)
+	await roll((getHeading() - 90), 50, 1.15);
+	await delay(1)
+	await roll((getHeading() - 90), 50, 1.05);
+	await delay(1)
+	await roll((getHeading() - 45), 50, 1.05);
+	await delay(1)
+	setMainLed({ r: 128, g: 0, b: 128 });
+	await Sound.ScienceFiction.Blaster.play(true);
 	// Programmer:               - Finish Text & Audio Commit - Date: 
+
 }
